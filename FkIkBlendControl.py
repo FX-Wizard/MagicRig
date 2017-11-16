a6902b2e4ffa86891b3cc051dc3f1116715a4a1f
FX Wizard <mh4596@gmail.com>
2017-11-16 22:50:51 +1100

    fixed control curves facing wrong way

--- FkIkBlendControl.py
+++ FkIkBlendControl.py
@@ -1,5 +1,6 @@
 def FkIkBlend(joints, name, pvOffset, switchCtrl, side = ""):
     '''Create an FK/IK controls with blend from joint list
+    Only works with RP solver
     
     Args:
         joints, list, 3 joints to create controls on
@@ -8,7 +9,7 @@ def FkIkBlend(joints, name, pvOffset, switchCtrl, side = ""):
         
         pvOffset, float, position of pole vector control in relation to the middle joint 
         
-        switchCtrl, str, the object the blend controll attributes will be added to
+        switchCtrl, str, the object the blend control attributes will be added to
         
     Kwargs:
         Side, str, Default = "", optional side prefix e.g. "L" or "R"
@@ -26,18 +27,21 @@ def FkIkBlend(joints, name, pvOffset, switchCtrl, side = ""):
     cmds.parent("IKJ_" + joints[2], "IKJ_" + joints[1])
     
     # FK Controls
-    for i in joints:
-        makeControl(i + "FK_Ctrl" + side, ctrlSize = 1.5, snapJoint = i, ctrlNormal = (0, 1, 0))
-        cmds.orientConstraint(i + "FK_Ctrl" + side, "FKJ_" + i, maintainOffset = True)
-        lockHide(i + "FK_Ctrl" + side, rotate = False)
+    makeControl(joints[0] + "FK_Ctrl" + side, ctrlSize = 1.5, snapJoint = joints[0], orientJoint = joints[1], ctrlNormal = (0, 1, 0))
+    cmds.orientConstraint(joints[0] + "FK_Ctrl" + side, "FKJ_" + joints[0], maintainOffset = True)
+    lockHide(joints[0] + "FK_Ctrl" + side, rotate = False)
+
+    makeControl(joints[1] + "FK_Ctrl" + side, ctrlSize = 1.5, snapJoint = joints[1], orientJoint = joints[2], ctrlNormal = (0, 1, 0))
+    cmds.orientConstraint(joints[1] + "FK_Ctrl" + side, "FKJ_" + joints[1], maintainOffset = True)
+    lockHide(joints[1] + "FK_Ctrl" + side, rotate = False)
     
     # IK Controls
-    cmds.ikHandle(name = "ik" + name, startJoint = "IKJ_" + joints[0], endEffector = "IKJ_" + joints[2], solver = "ikRPsolver")
-    makeControl(joints[2] + "IK_Ctrl" + side, ctrlSize = 2, snapJoint = joints[2], orientJoint =joints[1], ctrlNormal = (0, 1, 0))
-    cmds.parentConstraint(joints[2] + "IK_Ctrl" + side, "ik" + name, maintainOffset = True)
+    cmds.ikHandle(name = "ik" + name + side, startJoint = "IKJ_" + joints[0], endEffector = "IKJ_" + joints[2], solver = "ikRPsolver")
+    makeControl(joints[2] + "IK_Ctrl" + side, ctrlSize = 2, snapJoint = joints[2], orientJoint =joints[1], ctrlNormal = (0, 1, 0)) 
+    cmds.parentConstraint(joints[2] + "IK_Ctrl" + side, "ik" + name + side, maintainOffset = True)
     # Polevector
     makeControl(joints[1] + "PV_Ctrl" + side, 0.5, joints[1], None, ("z", pvOffset))
-    cmds.poleVectorConstraint(joints[1] + "PV_Ctrl" + side, "ik" + name)
+    cmds.poleVectorConstraint(joints[1] + "PV_Ctrl" + side, "ik" + name + side)
     lockHide(joints[1] + "PV_Ctrl" + side, translate = False)
 
     # Constraints
