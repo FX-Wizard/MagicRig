dca506148c169eef0ad1cdc7b4a721697b1f0892
FX Wizard <mh4596@gmail.com>
2018-03-27 09:15:38 +1100

    Update StretchyIK.py

--- StretchyIK.py
+++ StretchyIK.py
@@ -1,7 +1,7 @@
 import maya.cmds as cmds
 from math import sqrt, pow
 
-def makeStretchyIK(IKHandleName, jointOffset = 0):
+def makeStretchyIK(IKHandleName, jointOffset=0, controlObj=""):
 	"""Add stretch property to IK handle
 	
 	Args:
@@ -17,30 +17,34 @@ def makeStretchyIK(IKHandleName, jointOffset = 0):
 
 	# Get length of joint hierarchy
 	totalDistance = 0
-	cmds.select(startJoint, replace = True)
+	cmds.select(startJoint, replace=True)
 	
-	for i in range((len(jointList) + 1) - jointOffset):
-		j1 = cmds.joint(query = True, position = True, absolute = True)
-		cmds.pickWalk(direction = "down")
-		j2 = cmds.joint(query = True, position = True, absolute = True)
+	for i in range(len(jointList) - jointOffset):
+		j1 = cmds.joint(query=True, position=True, absolute=True)
+		cmds.pickWalk(direction="down")
+		j2 = cmds.joint(query=True, position=True, absolute=True)
 		distance = sqrt(pow(j1[0] - j2[0], 2) + pow(j1[1] - j2[1], 2) + pow(j1[2] - j2[2], 2))
 		totalDistance += distance
 	
     # ControlBox
-	ControlBox = cmds.group(IKHandleName, name = IKHandleName + "_StretchContol")
+	if controlObj:
+		cmds.select(controlObj, replace=True)
+	else:
+		controlObj = cmds.group(IKHandleName, name = IKHandleName + "_StretchContol")
 
 	cmds.addAttr(longName = "_____", attributeType = "double")
-	cmds.setAttr(ControlBox + "._____", edit = True, keyable = True, lock = True)
+	cmds.setAttr(controlObj + "._____", edit = True, keyable = True, lock = True)
 
 	cmds.addAttr(longName = "Stretch", attributeType = "double", min = 0, max = 1, defaultValue = 1)
-	cmds.setAttr(ControlBox + ".Stretch", edit = True, keyable = True)
+	cmds.setAttr(controlObj + ".Stretch", edit = True, keyable = True)
 
 	cmds.addAttr(longName = "Squish", attributeType = "double", min = 0, max = 1, defaultValue = 1)
-	cmds.setAttr(ControlBox + ".Squish", edit = True, keyable = True)
+	cmds.setAttr(controlObj + ".Squish", edit = True, keyable = True)
 	
 	#cmds.addAttr(longName = "Scale", attributeType = "double", min = 0, max = 1, defaultValue = 0)
-	#cmds.setAttr(ControlBox + ".Scale", edit = True, keyable = True)
-	
+	#cmds.setAttr(controlObj + ".Scale", edit = True, keyable = True)
+	cmds.select(clear=True)
+
 	# Create and connect utility nodes
 	distanceNode = cmds.shadingNode("distanceBetween", asUtility = True, name = "dist_" + IKHandleName)
 	conditionNode = cmds.shadingNode("condition", asUtility = True, name = "cond_" + IKHandleName)
@@ -63,12 +67,12 @@ def makeStretchyIK(IKHandleName, jointOffset = 0):
 	cmds.connectAttr((multiply1 + ".outputY"), (plusMinusAverage2 + ".input1D[0]"), force = True)
 	cmds.connectAttr((multiply1 + ".input2Z"), (plusMinusAverage2 + ".input1D[1]"), force = True)
 
-	cmds.connectAttr((ControlBox + ".Stretch"), (multiply1 + ".input1Y"), force = True)
+	cmds.connectAttr((controlObj + ".Stretch"), (multiply1 + ".input1Y"), force = True)
 
 	cmds.connectAttr((plusMinusAverage2 + ".output1D"), (conditionNode + ".colorIfFalseG"), force = True)
 	cmds.connectAttr((plusMinusAverage2 + ".output1D"), (conditionNode + ".firstTerm"), force = True)
-	cmds.connectAttr((ControlBox + ".Squish"), (conditionNode + ".secondTerm"), force = True)
-	cmds.connectAttr((ControlBox + ".Squish"), (conditionNode + ".colorIfTrueG"), force = True)
+	cmds.connectAttr((controlObj + ".Squish"), (conditionNode + ".secondTerm"), force = True)
+	cmds.connectAttr((controlObj + ".Squish"), (conditionNode + ".colorIfTrueG"), force = True)
 
 	cmds.connectAttr((conditionNode + ".outColorG"), (multiply2 + ".input1Y"), force = True)
 
