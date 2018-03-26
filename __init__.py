29573e42d2a0db65140c83ca3d603229d6446772
FX Wizard <mh4596@gmail.com>
2018-03-27 09:06:47 +1100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from . import AutoRig
+from . import Control
+from . import proxyObj
+from . import rigUtils
+from . import ui
+
+__all__ = ['AutoRig', 'Control', 'proxyObj', 'rigUtils', 'ui']
+
+__author__ = 'Luke Klaassen'
+__version__ = '0.0.2'
