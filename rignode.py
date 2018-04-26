67045ccab69fc61811330e88018280a196267d36
FX Wizard <mh4596@gmail.com>
2018-04-26 11:21:25 +1000

    Update rignode.py

--- rignode.py
+++ rignode.py
@@ -1,12 +1,7 @@
 import maya.cmds as cmds
 
-class MrNode:
-    def __init__(self,
-                 name,
-                 version="0.1",
-                 parent="",
-                 child="",
-                ):
+class MrNode(object):
+    def __init__(self, name, version="0.1", parent="", child=""):
         name = cmds.createNode("network", name=name)
         # default attributes
         self.addAttr("parent")
@@ -54,3 +49,60 @@ class MrNode:
 
     def setParent(self, parent):
         cmds.connectAttr(parent + ".child", self.name + ".parent")
+
+
+class MetaNode(object):
+    '''add metadata nodes to inherited classes'''
+    def __new__(cls, name, *args, **kwargs):
+        #if not name:
+        #    name = cls.__name__
+        cls.node = cmds.createNode("network", name=name)
+        cmds.addAttr(longName="parent", attributeType="message")
+        cmds.addAttr(longName="child", attributeType="message")
+        # set object type
+        cmds.addAttr(longName="object", dataType="string")
+        cmds.setAttr(name + ".object", cls.__name__, type="string", lock=True)
+        
+        return super(cls.__class__, cls).__new__(cls)
+
+
+    def __init__(self, name, *args, **kwargs):
+        print("super __init__ was called")
+        self.name = name
+    
+    
+    def addAttr(self, name, value=None, options="", lock=False):
+        '''adds attributes to MrNode
+        Args:
+        name -- name of attribute
+        kwargs:
+        options -- enum names (list)
+        value -- value to be added to attribute (default None)
+        '''
+        if options:
+            options = ":".join(options)
+            cmds.addAttr(longName=name, attributeType="enum", enumName=options)
+        elif type(value) == str:
+            cmds.addAttr(longName=name, dataType="string")
+        elif type(value) == int:
+            cmds.addAttr(longName=name, attributeType="long")
+        elif type(value) == float:
+            cmds.addAttr(longName=name, attributeType="double")
+        elif type(value) == bool:
+            cmds.addAttr(longName=name, attributeType="bool")
+        else:
+            cmds.addAttr(longName=name, attributeType="message")
+        if value:
+            self.modAttr(name, value, lock=lock)
+
+
+    def modAttr(self, name, value, lock=False):
+        if type(value) == str:
+            cmds.setAttr(self.name + "." + name, value, type="string", lock=lock)
+        else:
+            cmds.setAttr(self.name + "." + name, value, lock=lock)
+        
+
+    def setParent(self, parent):
+        cmds.connectAttr(parent + ".child", self.name + ".parent")
+
